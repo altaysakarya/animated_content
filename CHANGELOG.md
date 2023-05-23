515ce203df95e01e162126f4acd79cb9aa589e6f
altaysakarya <altaysakaryax@gmail.com>
2023-05-23 19:08:00 +0300

    license_readme_changelog_example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 1.0.0
 
 * Package created
+
+## 1.0.1
+
+* Add a example
+* License
--- example/main.dart
+++ example/main.dart
@@ -1,66 +1,10 @@
-# AnimatedContent
-
-The `AnimatedContent` package provides a widget for animating the content transitions in a Flutter application. It offers various animation types such as slide, fade, and combinations of both. The widget allows you to animate the child widget's entry and exit with customizable duration and animation styles.
-
-## Installation
-
-Add the following line to your `pubspec.yaml` file:
-
-```yaml
-dependencies:
-  animated_content: ^1.0.1
-```
-
-## Buy Me A Coffee ☕️
-
-<a href="https://www.buymeacoffee.com/altaysakarya" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
-
-### Features
-
-* The widget supports different animation types: `slideLeftToRight`, `slideRightToLeft`, `slideUpToDown`, `slideDownToUp`, `fadeSlideLeftToRight`, `fadeSlideRightToLeft`, `fadeUpToDown`, `fadeDownToUp`.
-* Customizable animation duration.
-* Supports a single child widget with animated content.
-* You can arrange multiple `AnimatedContent` widgets in a sequence.
-* Provides flexibility for automatic or button-triggered animations.
-
-### Usage
-
-Import the package:
-
-```dart
 import 'package:animated_content/animated_content.dart';
-```
+import 'package:flutter/material.dart';
 
-Wrap your content widget with the AnimatedContent widget:
+void main() {
+  runApp(const AnimatedContentExample());
+}
 
-```dart
-AnimatedContent(
-  child: YourContentWidget(),
-  duration: Duration(milliseconds: 500),
-  animation: AnimatedContentAnimation.slideLeftToRight,
-)
-```
-
-The available animation options are:
-
-* `slideLeftToRight`
-* `slideRightToLeft`
-* `slideUpToDown`
-* `slideDownToUp`
-* `fadeSlideLeftToRight`
-* `fadeSlideRightToLeft`
-* `fadeUpToDown`
-* `fadeDownToUp`
-
-You can customize the animation duration by providing a `Duration` object to the `duration` parameter.
-
-
-### Example
-
-<img src="https://github.com/altaysakarya/animated_content/blob/main/example_gif.gif?raw=true" height="400" alt="Example GIF">
-
-
-```dart
 class AnimatedContentExample extends StatelessWidget {
   const AnimatedContentExample({super.key});
 
@@ -105,5 +49,3 @@ class AnimatedContentExample extends StatelessWidget {
     );
   }
 }
-
-```
